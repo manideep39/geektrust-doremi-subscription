ccb2536ba174d70ce994f5dbed159b7ec75a9181
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2024-09-24 17:03:53 +0530

    refactor: Add final to TopUpConstant

--- src/main/java/com/example/geektrust/constant/TopUpConstant.java
+++ src/main/java/com/example/geektrust/constant/TopUpConstant.java
@@ -1,6 +1,6 @@
 package com.example.geektrust.constant;
 
-public class TopUpConstant {
+public final class TopUpConstant {
     public static final int FOUR_DEVICE_TOPUP_PRICE = 50;
     public static final int TEN_DEVICE_TOPUP_PRICE = 100;
 }
